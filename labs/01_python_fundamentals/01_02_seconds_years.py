068327a3d5dfbd7f9d01c53708649ca6048c1bff
marioS <elmaso@gmail.com>
2019-03-17 11:09:49 -0700

    Some code finetune

--- labs/01_python_fundamentals/01_02_seconds_years.py
+++ labs/01_python_fundamentals/01_02_seconds_years.py
@@ -3,3 +3,6 @@
 From the previous example, move your calculation of how many seconds in a year to a python executable script.
 
 '''
+
+seconds = 365 * 24 * 60 * 60
+print(seconds)
--- labs/01_python_fundamentals/01_06_hello5.py
+++ labs/01_python_fundamentals/01_06_hello5.py
@@ -4,3 +4,6 @@ Write the necessary code to display "Hello World!" 5 times.
 
 
 '''
+
+hello = "Hello World! " * 5
+print(hello)
